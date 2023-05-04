e1088c43e45ac506e0fd0560f102b0be63da7f96
Guillaume T <guillaume.turcotte2@ssc-spc.gc.ca>
2023-05-04 14:22:22 -0400

    code revamp for the new librairy update of llama_index

--- __init__.py
+++ __init__.py
@@ -10,9 +10,11 @@ from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from langchain.chat_models import AzureChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
-from llama_index import (GPTSimpleVectorIndex, LangchainEmbedding,
-                         LLMPredictor, PromptHelper, QuestionAnswerPrompt,
-                         ServiceContext, download_loader)
+from llama_index import (GPTVectorStoreIndex, LangchainEmbedding,
+                        PromptHelper, QuestionAnswerPrompt,
+                        ServiceContext, download_loader, StorageContext, load_index_from_storage)
+from llama_index.llm_predictor.chatgpt import ChatGPTLLMPredictor
+from llama_index.storage.storage_context import DEFAULT_PERSIST_DIR
 from llama_index.logger import LlamaLogger
 from langchain.prompts.chat import (
     AIMessagePromptTemplate,
@@ -30,7 +32,8 @@ app = Flask(__name__)
 key_vault_name          = os.environ["KEY_VAULT_NAME"]
 openai_endpoint_name    = os.environ["OPENAI_ENDPOINT_NAME"]
 deployment_name         = os.environ["OPENAI_DEPLOYMENT_NAME"]
-index_location          = os.getenv("INDEX_LOCATION", "/tmp/vector_index.json")
+_basepath              = "/tmp/"
+openai_api_version = "2023-03-15-preview" # this may change in the future
 
 kv_uri              = f"https://{key_vault_name}.vault.azure.net"
 azure_openai_uri    = f"https://{openai_endpoint_name}.openai.azure.com"
@@ -43,7 +46,7 @@ blob_service_client = BlobServiceClient.from_connection_string(client.get_secret
 openai.api_type    = os.environ["OPENAI_API_TYPE"]    = 'azure'
 openai.api_base    = os.environ["OPENAI_API_BASE"]    = azure_openai_uri
 openai.api_key     = os.environ["OPENAI_API_KEY"]     = client.get_secret("AzureOpenAIKey").value
-openai.api_version = os.environ["OPENAI_API_VERSION"] = '2023-03-15-preview' # this may change in the future
+openai.api_version = os.environ["OPENAI_API_VERSION"] = openai_api_version
 
 @app.route("/health", methods=["GET"])
 def health(): 
@@ -80,13 +83,13 @@ def query():
     service_context = _get_service_context(temperature)
 
     # Query ChatGPT / embeddings deployment(s)
-    index = get_index(temperature, service_context)
-    response = index.query(query, mode="embedding", 
-                           text_qa_template=_get_prompt_template(lang), 
-                           similarity_top_k=k, 
-                           response_mode="compact", 
-                           refine_template=_get_refined_prompt(lang))
-    
+    index = get_index(service_context)
+    query_engine = index.as_query_engine(mode="embedding", 
+                                        text_qa_template=_get_prompt_template(lang), 
+                                        similarity_top_k=k, 
+                                        response_mode="compact", 
+                                        refine_template=_get_refined_prompt(lang), service_context=service_context)
+    response = query_engine.query(query)
     #return StreamingResponse(index.query(query, streaming=True).response_gen)
     #print(response.get_formatted_sources())
     #print(service_context.llama_logger.get_logs())
@@ -96,13 +99,14 @@ def query():
     else:
         return jsonify({'query':query,'answer':str(response),'nodes_score':[node.score for node in response.source_nodes]})
        
-def get_index(temperature: float, service_context: ServiceContext) -> "GPTSimpleVectorIndex":
+def get_index(service_context: ServiceContext) -> "GPTVectorStoreIndex":
     # check if index file is present on fs ortherwise build it ...
-    if os.path.exists(index_location):
-        return GPTSimpleVectorIndex.load_from_disk(save_path=index_location, service_context=service_context)
+    if os.path.exists(DEFAULT_PERSIST_DIR):
+        storage_context = StorageContext.from_defaults(persist_dir=DEFAULT_PERSIST_DIR)
+        return load_index_from_storage(storage_context)
     else:
-        index = _build_index(temperature, service_context)
-        index.save_to_disk(index_location)
+        index = _build_index(service_context)
+        index.storage_context.persist()
         return index
 
 """
@@ -111,7 +115,7 @@ TODO: Move the two functions below to their own application service
 
 
 """       
-def _build_index(temperature: float, service_context: ServiceContext) -> "GPTSimpleVectorIndex":
+def _build_index(service_context: ServiceContext) -> "GPTVectorStoreIndex":
     logging.info("Creating index...")
     container_client = blob_service_client.get_container_client(container="unstructureddocs")
 
@@ -120,22 +124,21 @@ def _build_index(temperature: float, service_context: ServiceContext) -> "GPTSim
         _download_blob_to_file(blob_service_client, container_name="unstructureddocs", blob_name=blob.name)
     
     SimpleDirectoryReader  = download_loader("SimpleDirectoryReader")
-    documents = SimpleDirectoryReader(input_dir='/tmp/sscplus', recursive=True).load_data()
+    documents = SimpleDirectoryReader(input_dir='/tmp/sscplus2', recursive=True).load_data()
     #logging.info("The documents are:" + ''.join(str(x.doc_id) for x in documents))
 
-    return GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
+    return GPTVectorStoreIndex.from_documents(documents, service_context=service_context)
     
 def _download_blob_to_file(blob_service_client: BlobServiceClient, container_name, blob_name):
-    basepath = "/tmp/"
-    
+
     blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
 
     # azure function app only allows write to /tmp on the file system
-    isExist = os.path.exists(basepath + os.path.dirname(blob_name))
+    isExist = os.path.exists(_basepath + os.path.dirname(blob_name))
     if not isExist:
-        os.makedirs(basepath + os.path.dirname(blob_name))
+        os.makedirs(_basepath + os.path.dirname(blob_name))
 
-    with open(file=basepath + blob_name, mode="wb") as sample_blob:
+    with open(file=_basepath + blob_name, mode="wb") as sample_blob:
         download_stream = blob_client.download_blob()
         sample_blob.write(download_stream.readall())
 
@@ -146,17 +149,18 @@ def _get_service_context(temperature: str) -> "ServiceContext":
     chunk_size_limit = 1000 # token window size per document
     max_chunk_overlap = 20 # overlap for each token fragment
 
-    
-
     # using same dep as model name because of an older bug in langchains lib (now fixed I believe)
-    llm = AzureChatOpenAI(deployment_name=deployment_name, temperature=temperature, openai_api_version="2023-03-15-preview")
+    llm = AzureChatOpenAI(deployment_name=deployment_name, 
+                          temperature=temperature, 
+                          openai_api_version=openai_api_version)
     print(llm)
-    llm_predictor = LLMPredictor(llm=llm)
+    # https://gist.github.com/csiebler/32f371470c4e717db84a61874e951fa4
+    llm_predictor = ChatGPTLLMPredictor(llm=llm)
 
     prompt_helper = PromptHelper(max_input_size=max_input_size, num_output=num_output, max_chunk_overlap=max_chunk_overlap, chunk_size_limit=chunk_size_limit)
 
     # limit is chunk size 1 atm
-    embedding_llm = LangchainEmbedding(OpenAIEmbeddings(model="text-embedding-ada-002", chunk_size=1))
+    embedding_llm = LangchainEmbedding(OpenAIEmbeddings(model="text-embedding-ada-002", chunk_size=1, openai_api_version=openai_api_version))
 
     llama_logger = LlamaLogger()
 
