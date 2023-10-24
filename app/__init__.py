33e088bcc6f3c310121ceeb3a030726893a5fa3a
Guillaume Turcotte <leguille.ftw@gmail.com>
2023-10-24 08:15:55 -0400

    removed unused code (#34)
    * removed unused code
    * added syslog as info

--- app/__init__.py
+++ app/__init__.py
@@ -40,6 +40,9 @@ from llama_index.callbacks import CallbackManager, LlamaDebugHandler, CBEventTyp
 from app.prompts.qna import (get_chat_prompt_template, get_prompt_template,
                              get_refined_prompt)
 
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
 load_dotenv()
 
 app = Flask(__name__)
@@ -79,8 +82,7 @@ And we will keep the embedding chat history to a minimum (and we also restrict i
 _max_history = 5
 _max_embeddings_history = 1
 
-_default_index_name = os.getenv("INDEX_NAME", "2023-07-19")
-
+_default_index_name = os.getenv("INDEX_NAME", "2023-10-18")
 
 '''
 Bootstrap function to pre-load the vector index(ices)
@@ -118,9 +120,6 @@ def query():
         return jsonify({"error":"Request body must contain a query."}), 400
     else:
         query = body["query"]
-
-    if "index" in body:
-        index_name = body["index"]
            
     if "temp" in body:
         temperature = float(body["temp"])
