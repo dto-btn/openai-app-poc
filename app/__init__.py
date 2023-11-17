92febd9620319da1ea4d80e4ab7efb46ac15067e
Guillaume Turcotte <leguille.ftw@gmail.com>
2023-11-17 13:40:37 -0500

    added prompt fix and updated libraries version

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from langchain.chat_models import AzureChatOpenAI
 from langchain.embeddings import AzureOpenAIEmbeddings
-from llama_index import (GPTListIndex, LangchainEmbedding, PromptHelper,
+from llama_index import (GPTListIndex, PromptHelper,
                          QueryBundle, ServiceContext, StorageContext,
                          load_index_from_storage, set_global_service_context)
 from llama_index.callbacks import CallbackManager, LlamaDebugHandler
@@ -26,6 +26,11 @@ from llama_index.response_synthesizers import get_response_synthesizer
 from llama_index.response_synthesizers.type import ResponseMode
 from llama_index.retrievers import VectorIndexRetriever
 from llama_index.storage.storage_context import DEFAULT_PERSIST_DIR
+from llama_index.prompts.default_prompt_selectors import (
+    DEFAULT_REFINE_PROMPT_SEL,
+    DEFAULT_TEXT_QA_PROMPT_SEL,
+    DEFAULT_TREE_SUMMARIZE_PROMPT_SEL,
+)
 
 from app.prompts.qna import (get_chat_prompt_template, get_prompt_template,
                              get_refined_prompt)
@@ -42,7 +47,7 @@ key_vault_name          = os.environ["KEY_VAULT_NAME"]
 openai_endpoint_name    = os.environ["OPENAI_ENDPOINT_NAME"]
 
 models = {
-    "gpt-4": {"name": "gpt-4", "context_window": 8192, "index": {}, "service_context": ServiceContext },
+    "gpt-4": {"name": "gpt-4", "context_window": 8192, "index": {} },
 }
 
 kv_uri              = f"https://{key_vault_name}.vault.azure.net"
@@ -77,8 +82,7 @@ def _bootstrapIndex():
     for model in models.values():
         start = time.time()
         print("Loading up the index: {}... (with model: {})".format(_default_index_name, model))
-        model["service_context"] = _get_service_context(model["name"], model["context_window"])
-        set_global_service_context(model["service_context"])
+        set_global_service_context(_get_service_context(model["name"], model["context_window"]))
         # indices are pre-loaded with the same model they will be queried with
         model["index"] = _get_index(index_name=_default_index_name)
         end = time.time()
@@ -103,8 +107,6 @@ def chat():
     history = data.get('history', [])
     past_msg_incl = data.get('past_msg_incl', 10)
 
-    print(f"[QUERY] {query}")
-
     r = _generate_response(query, prompt=prompt, temp=temp, tokens=tokens, history=history, past_msg_incl=past_msg_incl)
 
     return jsonify(r)
@@ -122,8 +124,6 @@ def query():
     response_mode = ResponseMode.TREE_SUMMARIZE
     model_data = {}
     num_output = 800
-
-    print(body)
 
     if "query" not in body:
         return jsonify({"error":"Request body must contain a query."}), 400
@@ -181,17 +181,17 @@ def query():
             print("unable to load model, will use default", ex)
             return jsonify({"error": str(ex)}), 500
 
-    service_context = model_data["service_context"]
+    service_context = _get_service_context(model=model_data["name"], context_window=model_data["context_window"], temperature=temperature, num_output=num_output)
     query_bundle = query
 
     #prompt building, and query bundle
     if _has_history(history):
         print("using chat history for prompt and query")
         text_qa_template=get_chat_prompt_template(lang, _history_as_str(history))
-        #query_bundle = QueryBundle(
-        #    query,
-        #    custom_embedding_strs=_get_history_embeddings(history)
-        #)
+        query_bundle = QueryBundle(
+            query,
+            custom_embedding_strs=_get_history_embeddings(history)
+        )
     else:
         text_qa_template=get_prompt_template(lang)
 
@@ -200,10 +200,12 @@ def query():
     # configure response synthesizer
     response_synthesizer = get_response_synthesizer(response_mode=response_mode, 
                                                     service_context=service_context,
-                                                    text_qa_template=text_qa_template,
-                                                    summary_template=text_qa_template,
-                                                    simple_template=text_qa_template,
-                                                    refine_template=text_qa_template,
+                                                    #TODO: fix the prompt templates localisation
+                                                    # https://docs.llamaindex.ai/en/stable/examples/prompts/advanced_prompts.html
+                                                    #text_qa_template=text_qa_template,
+                                                    #summary_template=DEFAULT_TREE_SUMMARIZE_PROMPT_SEL,
+                                                    #simple_template=text_qa_template,
+                                                    #refine_template=text_qa_template,
                                                     )
 
     # assemble query engine
@@ -228,9 +230,7 @@ def query():
             'chat_history': str(history_enc, 'utf-8')
         }
 
-    print(f"[QUERY]: {r['query']}")
-    print(f"[ANSWER]:{r['answer']}")
-
+    logging.info(f"[QUERY]: {r['query']} [ANSWER]:{r['answer']}")
     return jsonify(r)
 
 def _get_index(index_name: str, storage_location: str = DEFAULT_PERSIST_DIR):
@@ -243,26 +243,23 @@ def _get_index(index_name: str, storage_location: str = DEFAULT_PERSIST_DIR):
     #return VectorStoreIndex.from_vector_store(vector_store=vector_store)
     return load_index_from_storage(storage_context)
 
-def _get_service_context(model: str, context_window: int, temperature: float = 0.7, num_output: int = 800) -> "ServiceContext":
-    chunk_overlap_ratio = 0.1 # overlap for each token fragment
-
+def _get_service_context(model: str, context_window: int, num_output: int = 800, temperature: float = 0.7,) -> "ServiceContext":
     # using same dep as model name because of an older bug in langchains lib (now fixed I believe)
     llm = _get_llm(model, temperature)
 
     llm_predictor = _get_llm_predictor(llm)
 
+    chunk_overlap_ratio = 0.1 # overlap for each token fragment
     prompt_helper = PromptHelper(context_window=context_window, num_output=num_output, chunk_overlap_ratio=chunk_overlap_ratio,)
 
     # limit is chunk size 1 atm
-    embedding_llm = LangchainEmbedding(
-        AzureOpenAIEmbeddings(
-            model="text-embedding-ada-002", api_key=api_key, azure_endpoint=azure_openai_uri),
-            embed_batch_size=1,)
+    embedding_llm = AzureOpenAIEmbeddings(
+            model="text-embedding-ada-002", api_key=api_key, azure_endpoint=azure_openai_uri)
 
     llama_debug = LlamaDebugHandler(print_trace_on_end=True)
     callback_manager = CallbackManager([llama_debug])
 
-    return ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper, embed_model=embedding_llm, callback_manager=callback_manager)
+    return ServiceContext.from_defaults(llm_predictor=llm_predictor, embed_model=embedding_llm, callback_manager=callback_manager, prompt_helper=prompt_helper)
 
 def _get_llm(model: str, temperature: float = 0.7):
     return AzureChatOpenAI(model=model,
@@ -390,7 +387,7 @@ def _generate_response(query: str, prompt: str, temp: float, tokens: int, histor
 
     content = response.choices[0].message.content
     role = response.choices[0].message.role
-    print(f"[ANSWER] {content} (role: {role})")
+    logging.info(f"[QUERY] {query} [ANSWER] {content} (role: {role})")
 
     r = {'message': [], 'created': '', 'history': [], 'id': '', 'model': '', 'object': '', 'usage': {'completion_tokens': int, 'prompt_tokens': int, 'total_tokens': int}}
 
